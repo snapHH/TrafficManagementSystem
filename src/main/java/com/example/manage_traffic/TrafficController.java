99e1b50de79e458620968e7794bf58103e3c467a
Sanket Hadap <123326672+snapHH@users.noreply.github.com>
2024-10-01 01:32:16 +0530

    trafficManagement

--- src/main/java/com/example/manage_traffic/TrafficController.java
+++ src/main/java/com/example/manage_traffic/TrafficController.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class TrafficController {
 
-    private final String HERE_API_KEY = "0ZIp4eECW7cDk-lYDvHO_x1Hgj8HF5rAx-95kZtKIdo"; // Get from HERE Traffic API
+    private final String HERE_API_KEY = "Your_API_Key"; // Get from HERE Traffic API
     private final String TRAFFIC_API_URL = "https://traffic.ls.hereapi.com/traffic/6.3/incidents.json" +
             "?bbox=37.7749,-122.4194;37.8044,-122.2711" +
             "&apiKey=" + HERE_API_KEY;
